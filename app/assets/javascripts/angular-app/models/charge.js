e718eb674f42efde87ea1ef7d33fed13b7b5fdcd
Joan Saez <janisaez@gmail.com>
2015-01-26 21:15:43 +0000

    fixing angular model paths

--- app/assets/javascripts/angular-app/models/charge.js
+++ app/assets/javascripts/angular-app/models/charge.js
@@ -3,7 +3,7 @@ angular
     .factory('Charge', function ($resource) {
 
         //var Charge = $resource('http://localhost:3000/api/v1/charges/:id.json', {id: '@id'},
-        var Charge = $resource('http://sheltered-brook-3657.herokuapp.com:3000/api/v1/charges/:id.json', {id: '@id'}, {
+        var Charge = $resource('http://sheltered-brook-3657.herokuapp.com/api/v1/charges/:id.json', {id: '@id'}, {
             update: {
                 method: 'PUT'
             }
--- app/assets/javascripts/angular-app/models/purchase.js
+++ app/assets/javascripts/angular-app/models/purchase.js
@@ -3,7 +3,7 @@ angular
     .factory('Purchase', function ($resource) {
 
         //var Purchase = $resource('http://localhost:3000/api/v1/purchases/:id.json', {id: '@id'},
-        var Purchase = $resource('http://sheltered-brook-3657.herokuapp.com:3000/api/v1/purchases/:id.json', {id: '@id'}, {
+        var Purchase = $resource('http://sheltered-brook-3657.herokuapp.com/api/v1/purchases/:id.json', {id: '@id'}, {
             update: {
                 method: 'PUT'
             }
